3d1913c408b94ab3e468495b1416f662fc13b7d5
E404ARPA <dam11.2020.jesuitas@gmail.com>
2020-10-18 11:25:11 +0200

    Initial commit

---
+++ app/src/main/java/com/example/e11/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.e11;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showToast(View view) {
+        Toast like = Toast.makeText(this, R.string.liki, Toast.LENGTH_LONG );
+        like.show();;
+    }
+}
